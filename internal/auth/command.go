de82ed48cafa1122a521342195173a452649df05
Drew Malin <drewmalin@gmail.com>
2021-03-16 19:48:25 -0700

    refresh tokens

---
+++ internal/auth/command.go
@@ -0,0 +1,17 @@
+package auth
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/brevdev/brev-go-cli/internal/cmdcontext"
+)
+
+func NewCmdLogin(context *cmdcontext.Context) *cobra.Command {
+	cmd := &cobra.Command{
+		Use: "login",
+		Run: func(cmd *cobra.Command, args []string) {
+			Login()
+		},
+	}
+	return cmd
+}
