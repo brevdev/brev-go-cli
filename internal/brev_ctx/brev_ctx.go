06f037d962521d754668ae19f5fc60445516b00b
naderlikeladder <nader.khalil@gmail.com>
2021-04-06 11:36:31 -0700

    add active_projects if not exists

--- internal/brev_ctx/brev_ctx.go
+++ internal/brev_ctx/brev_ctx.go
@@ -91,6 +91,9 @@ func (c *GlobalContext) GetProjectPaths() ([]string, error) {
 		return nil, fmt.Errorf("failed to read from %s: %s", getGlobalActiveProjectsPath(), err)
 	}
 	if !globalActiveProjectsFileExists {
+		paths := []string{}
+		files.OverwriteJSON(getGlobalActiveProjectsPath(), paths)
+		// return nil
 		return nil, nil
 	}
 
